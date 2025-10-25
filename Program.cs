f4d8c03c84538e4ba38bf8051be1c08508eba1d7
Gopankaj2018 <gopankaj@hotmail.com>
2025-10-25 17:51:36 +0000

    Initial commit: csharp project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+
+// auto-repo-mh6jp3qn-19 - C# Implementation
+// A simple Hello World program
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to auto-repo-mh6jp3qn-19");
+            
+            // Simple calculation
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int total = numbers.Sum();
+            Console.WriteLine($"Sum of numbers: {total}");
+        }
+    }
+}
